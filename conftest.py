a09fee6f8029fc6b21fc59bb30adff466582ec13
null <s_a_elizarov@mail.ru>
2026-08-05 15:20:37 +0300

    добавил файл с фикстурами

---
+++ conftest.py
@@ -0,0 +1,44 @@
+from datetime import datetime, timedelta
+from faker import Faker
+import pytest
+from core.clients.api_client import APIClient
+
+
+@pytest.fixture(scope="session")
+def api_client():
+    client = APIClient()
+    client.auth()
+    return client
+
+
+@pytest.fixture()
+def booking_dates():
+    today = datetime.today()
+    checkin_data = today + timedelta(days=10)
+    checkout_data = checkin_data + timedelta(days=5)
+
+    return {
+        'checkin': checkin_data.strftime('%Y-%m-%d'),
+        'checkout': checkout_data.strftime('%Y-%m-%d')
+    }
+
+
+@pytest.fixture()
+def generate_random_booking_data(booking_dates):
+    faker = Faker()
+    firstname = faker.first_name()
+    lastname = faker.first_name()
+    totalprice = faker.random_number(digits=3)
+    depositpaid = faker.boolean()
+    additionalneeds = faker.sentence()
+
+    data = {
+        'firstname': firstname,
+        'lastname': lastname,
+        'totalprice': totalprice,
+        'depositpaid': depositpaid,
+        'bookingdates': booking_dates,
+        'additionalneeds': additionalneeds
+
+    }
+    return data
